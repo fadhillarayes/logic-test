d3ba83e4be9bc42f6758404f6844a46702084880
FADHILLA DWI PRAMESWARY RAYES <fadhilla.dwi.p.r@gmail.com>
2021-11-05 16:14:41 +0700

    reverse number solved

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,11 +1,20 @@
-function checkVocal(string){
+function checkVocal(string) {
   // Start Code Here
-
+  const vocal = "aiueo";
+  var consonant = [];
+  for (var i = 0; i < string.length; i++) {
+    if (vocal.includes(string[i]) && !vocal.includes(string[i])) {
+      console.log("false");
+    } else {
+      return true;
+    }
+  }
+  return vocal;
 }
 
-console.log(checkVocal('iueoaAAaAA')) //output: true
-console.log(checkVocal('iuesdoaAAasfaAA')) //output: false
-console.log(checkVocal('iEEuoaAAaAA')) //output: true
-console.log(checkVocal('iuesdoaAAasfaAA')) //output: false
-console.log(checkVocal('iueoaAIIUUaAA')) //output: true
-console.log(checkVocal('iuedoaAAasfaAAmmCCB')) //output: false
+console.log(checkVocal("iueoaAAaAA")); //output: true
+console.log(checkVocal("iuesdoaAAasfaAA")); //output: false
+console.log(checkVocal("iEEuoaAAaAA")); //output: true
+console.log(checkVocal("iuesdoaAAasfaAA")); //output: false
+console.log(checkVocal("iueoaAIIUUaAA")); //output: true
+console.log(checkVocal("iuedoaAAasfaAAmmCCB")); //output: false
--- moneyCoin.js
+++ moneyCoin.js
@@ -1,14 +1,32 @@
-function moneyCoins (money) {
+function moneyCoins(money) {
   // Start Code Here
+  let count = 0;
+  let coins = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
 
+  var changer = function (index, value) {
+    var currentCoin = coins[index];
+    if (index === 0) {
+      if (value % currentCoin === 0) {
+        count++;
+      }
+      return;
+    }
+
+    while (value >= 0) {
+      changer(index - 1, value);
+      value -= currentCoin;
+    }
+  };
+  changer(coins.length - 1, money);
+  return count;
 }
 
 // Drive code
-console.log(moneyCoins(543))
+console.log(moneyCoins(543));
 //output : [ 500, 20, 20, 1, 1, 1 ]
 
-console.log(moneyCoins(7752))
+console.log(moneyCoins(7752));
 //output : [ 5000, 2000, 500, 200, 50, 1, 1 ]
 
-console.log(moneyCoins(37454))
+console.log(moneyCoins(37454));
 //output : [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
